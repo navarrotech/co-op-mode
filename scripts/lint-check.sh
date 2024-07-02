faf3f640d4125cac9484fe68f1c74c1c0b313816
Alex Navarro <alex@navarrotech.net>
2024-07-01 21:47:07 -0600

    More eslint god speak I guess

---
+++ scripts/lint-check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copyright © 2024 Navarrotech
+
+# cd analytics && yarn lint
+# cd ..
+cd api && yarn lint
+cd ..
+cd frontend && yarn lint
+cd ..
+cd gateway && yarn lint
+# cd ..
+# cd notifications && yarn lint
+# cd ..
+# cd routines && yarn lint
